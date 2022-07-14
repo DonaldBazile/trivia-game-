bc6f86297166c023e573b03c05632cc08f78423f
Donbazile07 <donbazile007@gmail.com>
2022-07-14 18:41:57 -0400

    trivia game created.

---
+++ trivia-script.js
@@ -0,0 +1,106 @@
+const questions = {
+    "questions": [
+        {
+            "question": "Elon Musk bought Apple in 2022.",
+            "answer": false
+        },
+        {
+            "question": "The capital of Florida is Miami.",
+            "answer": false
+        },
+        {
+            "question": "The capital of California is Sacramento.",
+            "answer": true
+        },
+        {
+            "question": "The United States was founded in 1998.",
+            "answer": false
+        },
+        {
+            "question": "Albert Einstein is Magician.",
+            "answer": false
+        },
+        {
+            "question": "The inventor of the first computer is Alan Turing.",
+            "answer": true
+        },
+        {
+            "question": "It takes a sloth two weeks to digest its food.",
+            "answer": true
+        },
+        {
+            "question": "A cow gives nearly 200,000 glasses of milk in a lifetime.",
+            "answer": true
+        },
+        {
+            "question": "Lebron James won 1 championship in collage.",
+            "answer": false
+        },
+        {
+            "question": "Patrick Mahomes plays for the Miami Dolphins.",
+            "answer": false
+        }
+    ]
+    
+}
+
+const question = document.getElementById('question')
+
+let playerScore = 0
+let questionIndex = 0
+let didReachLastQuestion = false
+question.innerText = questions.questions[questionIndex].question
+
+function handleTrue() {
+    const currentQuestionObject = questions.questions[questionIndex]
+    if (currentQuestionObject && currentQuestionObject.answer == true && didReachLastQuestion == false) {
+        playerScore += 1
+    }
+
+    nextQuestion()
+}
+
+function handleFalse() {
+    const currentQuestionObject = questions.questions[questionIndex]
+    if (currentQuestionObject && currentQuestionObject.answer == false && didReachLastQuestion == false) {
+        playerScore += 1
+    }
+
+    nextQuestion()
+}
+
+function nextQuestion() {
+    questionIndex += 1
+    if (questionIndex >= questions.questions.length) {
+        didReachLastQuestion = true
+        const yourFinalScore = `Your score is ${playerScore} / ${questions.questions.length}.`
+        question.innerText = yourFinalScore
+        shouldHideAnswerButtons(true)
+    } else {
+        question.innerText = questions.questions[questionIndex].question
+    }
+}
+
+function replay() {
+    shouldHideAnswerButtons(false)
+    playerScore = 0
+    questionIndex = 0
+    didReachLastQuestion = false
+    question.innerText = questions.questions[questionIndex].question
+}
+
+function shouldHideAnswerButtons(flag) {
+    const trueButton = document.getElementById('true-button')
+    const falseButton = document.getElementById('false-button')
+    const replayButton = document.getElementById('replay-button')
+
+    if (flag == true) {
+        replayButton.style.opacity = 1
+        trueButton.style.opacity = 0
+        falseButton.style.opacity = 0
+    } else {
+        replayButton.style.opacity = 0
+        trueButton.style.opacity = 1
+        falseButton.style.opacity = 1
+    }
+}
